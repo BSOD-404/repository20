c208a9663748d86cf1e92f2d0138a95e0535ae53
Bohdan Melenchuk <jailedkurtanogle@gmail.com>
2017-07-28 16:31:28 +0000

    alpha

---
+++ js/jquery-sample.js
@@ -0,0 +1,10 @@
+$(function() {
+    let divs = document.getElementsByTagName('div');
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].innerHTML = `<h1>Hello jQuery! ${i} </h1>`;
+    }
+
+    $('div').click(function() {
+        
+    });
+});
